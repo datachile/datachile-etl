5186411e52deb634f1b1c9c0034f6e23c9305f61
Pablo H. Paladino <paladinop@gmail.com>
2017-07-07 16:08:28 -0400

    electoral information

--- libs/commons.py
+++ libs/commons.py
@@ -40,6 +40,7 @@ def download_file(remote_path,local_path,file_name):
     
     with open(local_file, 'rb') as local_csv:
         result = chardet.detect(local_csv.read())
+
     return pd.read_csv(local_file,delimiter=",",encoding = result['encoding'])
 
 def extract_zip_file(local_path,file_name):
